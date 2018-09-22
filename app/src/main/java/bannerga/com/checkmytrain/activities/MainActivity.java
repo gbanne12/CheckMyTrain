ea467fc855337014055375de21d62ec1c746c31e
gbanne12 <gbannerman14986@gmail.com>
2018-09-22 10:16:17 +0100

    Correct name used for layout file in the Main Activity.

--- app/src/main/java/bannerga/com/checkmytrain/activities/MainActivity.java
+++ app/src/main/java/bannerga/com/checkmytrain/activities/MainActivity.java
@@ -1,11 +1,7 @@
 package bannerga.com.checkmytrain.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_design);
+        setContentView(R.layout.activity_request_configuration);
 
     }
 
